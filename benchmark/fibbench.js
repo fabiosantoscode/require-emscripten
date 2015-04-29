b9b84535e6db6ebdd4c3d314e73267630c14ba13
Fábio Santos <fabiosantosart@gmail.com>
2015-04-29 01:02:33 +0100

    fix benchmark

--- benchmark/fibbench.js
+++ benchmark/fibbench.js
@@ -6,13 +6,13 @@ var fibJs = require('./fib.js')
 
 console.log('calculating 10000 fibonaccis in requireEmscripten\'d C')
 var start = +new Date()
-for (var i = 0; i < 10000; i++) {
-    var result = fib(1000)
+for (var i = 0; i < 1000000; i++) {
+    var result = fib(40)
 }
 console.log('Took ' + (+new Date() - start) + 'ms')
 console.log('calculating 10000 JS fibonaccis in java scripts')
 var start = +new Date()
-for (var i = 0; i < 10000; i++) {
-    var result = fibJs(1000)
+for (var i = 0; i < 1000000; i++) {
+    var result = fibJs(40)
 }
 console.log('Took ' + (+new Date() - start) + 'ms')
